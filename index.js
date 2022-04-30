8b15682a7594698feab559be1b4e6b1b765955a6
Arnaldo Borda <aborda001@gmail.com>
2022-04-30 10:20:04 -0400

    Sending json

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: '$10'
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
