38561754ffd4c3aeabbec84c2d373dd15925a996
Arnaldo Borda <aborda001@gmail.com>
2022-05-07 21:57:27 -0400

    Using query params

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.get('/products/:id', (req, res) => {
   });
 });
 
+app.get("/users", (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send("No hay parametros");
+  }
+});
+
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
   res.json({
