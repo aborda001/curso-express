64c86f6323b16c605cf16b0aff3911f8ad6bc0df
Arnaldo Borda <aborda001@gmail.com>
2022-05-08 19:38:48 -0400

    Implementation crud services of products

---
+++ services/products.service.js
@@ -0,0 +1,73 @@
+const faker = require('faker');
+
+class ProductsService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+    for (let i = 0; i < limit; i++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+      });
+    }
+  }
+
+  create(data) {
+    const { name, price, image } = data;
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      name,
+      price,
+      image,
+    };
+
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  find() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find((product) => product.id === id);
+  }
+
+  update(id, chnages) {
+    const index = this.products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+
+    const product = this.products[index];
+
+    this.products[index] = {
+      ...product,
+      ...chnages,
+    };
+    return this.products[index];
+  }
+
+  delete(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+
+    this.products.splice(index, 1);
+
+    return {
+      id,
+    };
+  }
+}
+
+module.exports = ProductsService;
