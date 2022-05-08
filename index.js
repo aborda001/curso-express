4cc47300da24c7b3335ca24d485b0859eed97895
Arnaldo Borda <aborda001@gmail.com>
2022-05-07 22:08:33 -0400

    Using faker for create fake data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const PORT = 3000;
 
@@ -7,36 +8,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Product 1',
-      price: '$1.99'
-    },
-    {
-      name: 'Product 2',
-      price: '$2.99'
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
 });
+
+app.get('/products/filter', (req, res) => {
+  res.send('Filter products');
+});
+
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.json({
-    id ,
+    id,
     name: 'Product 1',
-    price: '$1.99'
+    price: '$1.99',
   });
 });
 
-app.get("/users", (req, res) => {
-  const {limit, offset} = req.query;
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
   if (limit && offset) {
     res.json({
       limit,
-      offset
+      offset,
     });
   } else {
-    res.send("No hay parametros");
+    res.send('No hay parametros');
   }
 });
 
@@ -46,7 +54,7 @@ app.get('/categories/:categoryId/products/:productId', (req, res) => {
     categoryId,
     productId,
     name: 'Product 1',
-    price: '$1.99'
+    price: '$1.99',
   });
 });
 
