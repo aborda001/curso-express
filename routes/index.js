06baa3fea3f2c7ad78104996fea1c051bd5b9b86
Arnaldo Borda <aborda001@gmail.com>
2022-05-07 22:32:17 -0400

    Main route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
+const express = require('express');
 const productsRouter = require('./products.routes');
 const usersRouter = require('./users.routes');
 const categoriesRouter = require('./categories.routes');
 
 function routerApi (app) {
-  app.use('/api/products', productsRouter);
-  app.use('/api/users', usersRouter);
-  app.use('/api/categories', categoriesRouter);
+
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
